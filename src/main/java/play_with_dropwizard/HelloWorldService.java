4d5b70849701b08ce960b3049ac7eedee4832e21
Simon Katz <sk@nomistech.com>
2013-10-27 17:17:12 +0000

    Add logging of HTTP ports at startup.

--- src/main/java/play_with_dropwizard/HelloWorldService.java
+++ src/main/java/play_with_dropwizard/HelloWorldService.java
@@ -3,6 +3,9 @@ package play_with_dropwizard;
 import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
+import com.yammer.dropwizard.config.HttpConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import play_with_dropwizard.health.TemplateHealthCheck;
 import play_with_dropwizard.resources.HelloWorldResource;
 
@@ -14,15 +17,24 @@ public class HelloWorldService extends Service<HelloWorldConfiguration> {
 
     @Override
     public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
-        bootstrap.setName("hello-world");
-    }
+        bootstrap.setName("hello-world"); // (does not need to be same as yml file name)
+     }
 
     @Override
     public void run(HelloWorldConfiguration configuration,
                     Environment environment) {
+        Logger logger = LoggerFactory.getLogger("com.nomistech.play_with_dropwizard");
+
         final String template = configuration.getTemplate();
         final String defaultName = configuration.getDefaultName();
         environment.addResource(new HelloWorldResource(template, defaultName));
         environment.addHealthCheck(new TemplateHealthCheck(template));
+
+        HttpConfiguration foo = configuration.getHttpConfiguration();
+        logger.info("httpConfiguration = ["
+                + configuration.getHttpConfiguration().getPort()
+                + ", "
+                + configuration.getHttpConfiguration().getAdminPort()
+                + "]");
     }
 }
